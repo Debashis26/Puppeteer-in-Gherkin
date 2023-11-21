701ee203d2868e81eb70dde9f0bc5970d8e21435
Debashis Badajena <140513278+Debashis26@users.noreply.github.com>
2023-11-21 15:58:41 +0530

    steps added for google_search_feature

--- src/e2e/features/google_search.feature
+++ src/e2e/features/google_search.feature
@@ -2,5 +2,5 @@ Feature: Google Search
 
   Scenario: Perform a search on Google
     Given I am on the Google search page
-    When I search for google.com
+    When I search for "debashisbadajena"
     Then I should see search results
--- src/e2e/steps/google_search.steps.ts
+++ src/e2e/steps/google_search.steps.ts
@@ -1,19 +1,26 @@
-import { Given, Then,When,browser,page } from "../support/puppeteerSupport";
+import { Given, Then, When, browser, page } from "../support/puppeteerSupport";
 
-Given('I am on the Google search page', async function () {
-  await page.goto('https://www.google.com');
-});
-
-When('I search for google.com', async function () {
+Given("I am on the Google search page", async function () {
   if (page) {
-    await page.type('input[name="q"]', 'google.com');
-    await page.keyboard.press('Enter');
+    await page.setViewport({ width: 1360, height: 1080 });
+    await page.goto("https://www.google.com");
   }
 });
 
-Then('I should see search results', async function () {
+When('I search for "debashisbadajena"', async function () {
   if (page) {
-    await page.waitForSelector('#search');
+    const searchSelector = 'textarea[title="Search"]';
+    await page.waitForSelector(searchSelector);
+    await page.click(searchSelector, { clickCount: 1 });
+    await page.type(searchSelector, "debashis badajena");
+    // await page.type(searchSelector, "");
+    await page.keyboard.press("Enter");
   }
-  
+});
+
+Then("I should see search results", async function () {
+  if (page) {
+    await page.waitForSelector("h3");
+    const searchResults = await page.$$("h3");
+  }
 });
